9f3aa057cef4d7d7590b30149d2b3ad1f64e8bc9
marcos7530 <88636203+marcos7530@users.noreply.github.com>
2024-12-11 00:48:15 -0300

    se terminaron de hacer las views del menu

---
+++ SistemaDeVenta/SistemaVenta.IOC/Dependencias.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencias
+    {
+
+        public static void InyectarDependecia(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<DbventaContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("CadenaSQL")));
+
+        }
+
+
+    }
+}
